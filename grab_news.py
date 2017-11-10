edd9c893f153eedaad3c16ce7f2e8dff5900b9b1
Sardor Muminov <sardor@insilicogen.com>
2017-11-10 12:12:24 +0900

    Initial commit

---
+++ grab_news.py
@@ -0,0 +1,33 @@
+import os
+import feedparser
+import googletrans
+import redis
+import logging
+
+redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+rss_url = os.environ.get('URL', 'https://hnrss.org/newest?points=300')
+logger = logging.getLogger('bot')
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main():
+    r = redis.from_url(redis_url)
+    feed = feedparser.parse(rss_url)
+    translator = googletrans.Translator()
+
+    for entry in feed['entries']:
+
+        if 'show hn:' in entry['title'].lower():
+            continue
+
+        if 'ask hn:' in entry['title'].lower():
+            continue
+
+        t = translator.translate(entry['title'], dest='uz', src='en')
+        text = '{title}\n\n{link}'.format(title=t.text, link=entry['link'])
+        logger.info('Saving post {text} to redis...'.format(text=text))
+        r.sadd('posts', text)
+
+
+if __name__ == '__main__':
+    main()
---
+++ post_to_channel.py
@@ -0,0 +1,23 @@
+import os
+import logging
+import asyncio
+import aiotg
+import redis
+
+api_token = os.environ.get('API_TOKEN')
+bot_name = os.environ.get('BOT_NAME')
+bot = aiotg.Bot(api_token=api_token, name=bot_name)
+channel = bot.channel(os.environ.get('CHANNEL_NAME', '@BozorvoyNews'))
+redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+logger = logging.getLogger('bot')
+logging.basicConfig(level=logging.DEBUG)
+
+
+async def main():
+    r = redis.from_url(redis_url)
+    post = r.spop('posts')
+    logger.info('Publishing post {title} to channel...'.format(title=post))
+    await channel.send_text(post.decode('utf-8'))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
