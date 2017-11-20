c6d8995ed9694f65389f6cd865806895a4743338
Sardor Muminov <sardor@insilicogen.com>
2017-11-20 16:47:48 +0900

    Add shortening urls feature

--- grab_news.py
+++ grab_news.py
@@ -3,11 +3,31 @@ import feedparser
 import googletrans
 import redis
 import logging
+import requests
 
 redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
 rss_url = os.environ.get('URL', 'https://hnrss.org/frontpage')
+bitly_api_url = os.environ.get('BITLY_API_URL', 'https://api-ssl.bitly.com/v3/shorten')
+bitly_access_token = os.environ.get('BITLY_ACCESS_TOKEN')
 logger = logging.getLogger('bot')
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+
+
+def shorten_url(url):
+    params = {
+        'access_token': bitly_access_token,
+        'longUrl': url,
+        'domain': 'j.mp'
+    }
+    response = requests.get(bitly_api_url, params=params)
+    short_url = None
+
+    try:
+        short_url = response.json()['data']['url']
+    except:
+        pass
+
+    return short_url
 
 
 def main():
@@ -24,16 +44,12 @@ def main():
             continue
 
         t = translator.translate(entry['title'], dest='uz', src='en')
-        text = '{title}\n\n{link}'.format(title=t.text, link=entry['link'])
+        link = shorten_url(entry['link'])
+        text = '{title}\n\n{link}'.format(title=t.text, link=link)
 
-        if r.sismember('published_posts', text):
-            logger.info(
-                'Post {text} already published, skipping...'.format(
-                    text=text))
-            continue
-
-        logger.info('Saving post {text} to redis...'.format(text=text))
-        r.sadd('unpublished_posts', text)
+        if not r.sismember('published_posts', text):
+            logger.info(text + '\n')
+            r.sadd('unpublished_posts', text)
 
 
 if __name__ == '__main__':
